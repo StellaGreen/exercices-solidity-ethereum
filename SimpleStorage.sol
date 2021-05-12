1772cf1889ddd23185c8fac7937663ba55c788f2
StellaGreen <vickysoler5@gmail.com>
2021-05-12 10:13:52 +0200

    add correction

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -3,24 +3,15 @@
 pragma solidity ^0.8.0;
 
 contract SimpleStorage {
-    
-    address private _owner;
-    uint256 private _storedData;
-    
-    constructor(address owner_) {
-        _owner = owner_;
-    }
-    
-    function setStoredData(uint256 value) public {
-        require (msg.sender == _owner, "setStoredData : only _owner can modify storedData");
-        _storedData = 0;
-       value += _storedData;
-    }
 
-    
-    function storedData() public view returns(uint256){
+    uint256 private _storedData;
+
+    function storedData() public view returns (uint256){
         return _storedData;
     }
-    
+
+    function setStoredData(uint256 value) {
+        _storedData = value;
+    } 
     
 }
